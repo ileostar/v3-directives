1b8c28ffb405b8aa7f184c98501c8eab2fa33753
ileostar <hi@leostar.top>
2024-02-22 21:32:17 +0800

    fix: fix action build bug

--- docs/.vitepress/src/docs.ts
+++ docs/.vitepress/src/docs.ts
@@ -10,10 +10,5 @@ export const docsConfig: DocsConfig = {
   lastUpdated: true,
   appearance: 'dark',
   outline: [2, 3],
-<<<<<<< HEAD
   ignoreDeadLinks: true,
 }
-=======
-  ignoreDeadLinks: true
-};
->>>>>>> f818ded7189560953cf91ccf1de3f6d1d9b54c3c
